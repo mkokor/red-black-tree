d98c6da74a5a5cf3b248bd061c538f99a591cfb8
Matija Kokor <matijakokorr@gmail.com>
2023-11-07 11:20:16 +0100

    design of red black tree class refactored

--- RedBlackTree/RedBlackTree.cs
+++ RedBlackTree/RedBlackTree.cs
@@ -1,88 +1,11 @@
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace RedBlackTree
 {
     public class RedBlackTree<TKeyValue> where TKeyValue : IComparable
     {
-        public Node? Root { get; private set; } // If a tree is empty, root is a null (it is sentinel node).
-
-        public List<Node> Nodes { get; private set; }
-        public int NumberOfElements { get; private set; }
-
-        public RedBlackTree()
-        {
-            Root = null;
-            NumberOfElements = 0;
-            Nodes = new List<Node>();
-        }
-
-        private static bool IsLessThan(TKeyValue firstOperand, TKeyValue secondOperand)
-        {
-            return firstOperand.CompareTo(secondOperand) < 0;
-        }
-
-        public override string ToString()
-        {
-            return Nodes.Select(node => node.ToString() + (node.Index == Nodes.Count - 1 ? "" : "\n"))
-                        .Aggregate((result, value) => result + value);
-        }
-
-        public void Insert(TKeyValue value)
-        {
-            Node? currentNode = Root;
-            Node? currentNodeParent = null;
-            while (currentNode is not null)
-            {
-                currentNodeParent = currentNode;
-                currentNode = IsLessThan(value, currentNode.KeyValue) ? currentNode.LeftChild : currentNode.RightChild;
-            }
-            Node newNode = new(value, currentNodeParent);
-            if (currentNodeParent is null)
-                Root = newNode;
-            else if (IsLessThan(newNode.KeyValue, currentNodeParent.KeyValue))
-                currentNodeParent.LeftChild = newNode;
-            else
-                currentNodeParent.RightChild = newNode;
-            Nodes.Add(newNode);
-            newNode.Index = Nodes.Count - 1;
-            // Fix!
-        }
-
-        public void RotateLeft(Node criticalNode)
-        {
-            Node rotationNode = criticalNode.RightChild ?? throw new InvalidOperationException("Unable to do left rotation.");
-            criticalNode.RightChild = rotationNode?.LeftChild;
-            if (rotationNode?.LeftChild is not null)
-                rotationNode.LeftChild.Parent = criticalNode;
-            rotationNode.Parent = criticalNode.Parent; // The rotation node can not be a null (first line is throwing exception in case it is).
-            if (criticalNode.Parent is null)
-                Root = rotationNode;
-            else if (criticalNode == criticalNode.Parent.LeftChild)
-                criticalNode.Parent.LeftChild = rotationNode;
-            else
-                criticalNode.Parent.RightChild = rotationNode;
-            rotationNode.LeftChild = criticalNode;
-            criticalNode.Parent = rotationNode;
-        }
-
-        public void RotateRight(Node criticalNode)
-        {
-            Node rotationNode = criticalNode.LeftChild ?? throw new InvalidOperationException("Unable to do right rotation.");
-            criticalNode.LeftChild = rotationNode?.RightChild;
-            if (rotationNode?.RightChild is not null)
-                rotationNode.RightChild.Parent = criticalNode;
-            rotationNode.Parent = criticalNode.Parent; // The rotation node can not be a null (first line is throwing exception in case it is).
-            if (criticalNode.Parent is null)
-                Root = rotationNode;
-            else if (criticalNode == criticalNode.Parent.LeftChild)
-                criticalNode.Parent.LeftChild = rotationNode;
-            else
-                criticalNode.Parent.RightChild = rotationNode;
-            rotationNode.RightChild = criticalNode;
-            criticalNode.Parent = rotationNode;
-        }
-
-        public class Node
+        private class Node
         {
             public int Index { get; set; } // This property is made for testing purposes.
             public TKeyValue KeyValue { get; set; }
@@ -106,11 +29,125 @@ namespace RedBlackTree
                 string ParentString = Parent is null ? "null" : $"{Parent.Index}";
                 return $"Index: {Index}; Value: {KeyValue}; Color: {Color}; LeftChild: {LeftChildString}; RightChild: {RightChildString}; Parent: {ParentString}";
             }
+        }
 
-            public enum NodeColor
+        public enum NodeColor
+        {
+            RED,
+            BLACK
+        }
+
+        private Node? root; // If a tree is empty, root is a null (it is sentinel node).
+
+        private int numberOfElements;
+
+        public RedBlackTree()
+        {
+            root = null;
+            numberOfElements = 0;
+        }
+
+        private List<Node>? GetNodesInOrder(Node subtreeRoot)
+        {
+            if (subtreeRoot is null)
+                return null;
+            List<Node> nodes = new();
+            return nodes;
+        }
+
+        private static bool IsLessThan(TKeyValue firstOperand, TKeyValue secondOperand)
+        {
+            return firstOperand.CompareTo(secondOperand) < 0;
+        }
+
+        public override string ToString()
+        {
+            return "";/*Nodes.Select(node => node.ToString() + (node.Index == Nodes.Count - 1 ? "" : "\n"))
+                        .Aggregate((result, value) => result + value);*/
+        }
+
+        public void Insert(TKeyValue value)
+        {
+            Node? currentNode = root;
+            Node? currentNodeParent = null;
+            while (currentNode is not null)
             {
-                RED,
-                BLACK
+                currentNodeParent = currentNode;
+                currentNode = IsLessThan(value, currentNode.KeyValue) ? currentNode.LeftChild : currentNode.RightChild;
+            }
+            Node newNode = new(value, currentNodeParent);
+            if (currentNodeParent is null)
+                root = newNode;
+            else if (IsLessThan(newNode.KeyValue, currentNodeParent.KeyValue))
+                currentNodeParent.LeftChild = newNode;
+            else
+                currentNodeParent.RightChild = newNode;
+            newNode.Index = numberOfElements;
+            numberOfElements++;
+            // Fix!
+        }
+
+        private void RotateLeft(Node criticalNode)
+        {
+            Node rotationNode = criticalNode.RightChild ?? throw new InvalidOperationException("Unable to do left rotation.");
+            criticalNode.RightChild = rotationNode?.LeftChild;
+            if (rotationNode?.LeftChild is not null)
+                rotationNode.LeftChild.Parent = criticalNode;
+            rotationNode.Parent = criticalNode.Parent; // The rotation node can not be a null (first line is throwing exception in case it is).
+            if (criticalNode.Parent is null)
+                root = rotationNode;
+            else if (criticalNode == criticalNode.Parent.LeftChild)
+                criticalNode.Parent.LeftChild = rotationNode;
+            else
+                criticalNode.Parent.RightChild = rotationNode;
+            rotationNode.LeftChild = criticalNode;
+            criticalNode.Parent = rotationNode;
+        }
+
+        private void RotateRight([DisallowNull] Node criticalNode)
+        {
+            Node rotationNode = criticalNode.LeftChild ?? throw new InvalidOperationException("Unable to do right rotation.");
+            criticalNode.LeftChild = rotationNode?.RightChild;
+            if (rotationNode?.RightChild is not null)
+                rotationNode.RightChild.Parent = criticalNode;
+            rotationNode.Parent = criticalNode.Parent; // The rotation node can not be a null (first line is throwing exception in case it is).
+            if (criticalNode.Parent is null)
+                root = rotationNode;
+            else if (criticalNode == criticalNode.Parent.LeftChild)
+                criticalNode.Parent.LeftChild = rotationNode;
+            else
+                criticalNode.Parent.RightChild = rotationNode;
+            rotationNode.RightChild = criticalNode;
+            criticalNode.Parent = rotationNode;
+        }
+
+        private void FixTreeStructure([DisallowNull] Node criticalNode)
+        {
+            while (criticalNode.Parent is not null && criticalNode.Parent.Color == NodeColor.RED)
+            {
+                Node? criticalNodeUncle;
+                if (criticalNode.Parent == criticalNode.Parent.Parent?.LeftChild)
+                {
+                    criticalNodeUncle = criticalNode.Parent.RightChild;
+                    if (criticalNodeUncle?.Color == NodeColor.RED)
+                    {
+                        criticalNode.Parent.Color = NodeColor.BLACK;
+                        criticalNodeUncle.Color = NodeColor.BLACK;
+                        criticalNode.Parent.Parent.Color = NodeColor.RED;
+                        criticalNode = criticalNode.Parent.Parent;
+                    }
+                    else
+                    {
+                        if (criticalNode == criticalNode.Parent.RightChild)
+                        {
+                            criticalNode = criticalNode.Parent;
+                            RotateLeft(criticalNode);
+                        }
+                        criticalNode.Parent.Color = NodeColor.BLACK;
+                        criticalNode.Parent.Parent.Color = NodeColor.RED;
+                        RotateRight(criticalNode.Parent.Parent);
+                    }
+                }
             }
         }
     }
